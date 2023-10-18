0944da46b63b6feb53b09984e829bbaafad61e04
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-10-17 22:49:11 -0400

    Ended section 2 with a simple todo list react piece

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -3,11 +3,14 @@ import PathParameters from "./PathParameters";
 import Classes from "./Classes";
 import Styles from './Styles';
 import ConditionalOutput from "./ConditionalOutput";
+import TodoItem from "./Todos/TodoItem";
+import TodoList from "./Todos/TodoList";
 
 function Assignment3() {
     return (
         <div>
             <h1>Assignment 3</h1>
+            <TodoList />
             <ConditionalOutput />
             <Styles />
             <Classes />
