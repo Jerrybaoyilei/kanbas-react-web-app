830cf715684bf4b8778430858b1b318053fbf3bb
Jerrybaoyilei <jerrybaoyilei@qq.com>
2023-11-21 10:26:32 +0800

    a5 before 3.5

--- kanbas-node-server-app/app.js
+++ kanbas-node-server-app/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import Hello from './hello.js';
 import Lab5 from './lab5.js';
+import cors from "cors";
+
 const app = express();
+app.use(cors());
 // app.get('/hello', (req, res) => { res.send('Life is good!') })
 // app.get('/', (req, res) => { res.send("Welcome to Full Stack Development!") })
 Lab5(app)
--- kanbas-node-server-app/lab5.js
+++ kanbas-node-server-app/lab5.js
@@ -9,19 +9,48 @@ const assignment = {
 
 const todos = [
     { id: 1, title: "Task 1", completed: false },
-    { id: 2, title: "Task 2", completed: false },
+    { id: 2, title: "Task 2", completed: true },
     { id: 3, title: "Task 3", completed: false },
-    { id: 4, title: "Task 4", completed: false },
+    { id: 4, title: "Task 4", completed: true },
 ];
 
 const Lab5 = (app) => {
     app.get("/a5/todos", (req, res) => {
+        const { completed } = req.query;
+        if (completed !== undefined) {
+            // Query parameter is a string, not a boolean
+            const completedTodos = todos.filter(
+                (t) => t.completed.toString() === completed);
+            res.json(completedTodos);
+            return;
+        }
+        res.json(todos);
+    });
+    app.get("/a5/todos/create", (req, res) => {
+        const newTodo = {
+            id: new Date().getTime(),
+            title: "New Task",
+            completed: false,
+        };
+        todos.push(newTodo);
         res.json(todos);
     });
     app.get("/a5/todos/:id", (req, res) => {
         const { id } = req.params;
         const todo = todos.find((t) => t.id === parseInt(id));
         res.json(todo);
+    });
+    app.get("/a5/todos/:id/delete", (req, res) => {
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        todos.splice(todos.indexOf(todo), 1);
+        res.json(todos);
+    });
+    app.get("/a5/todos/:id/title/:title", (req, res) => {
+        const { id, title } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        todo.title = title;
+        res.json(todos);
     });
     app.get("/a5/assignment", (req, res) => {
         res.json(assignment);
